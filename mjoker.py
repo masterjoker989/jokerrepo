a7f65de84a4cf108d9b05721e5dab6765457916b
masterjoker989 <46088660+masterjoker989@users.noreply.github.com>
2018-12-23 22:43:24 +0530

    Update mjoker.py

--- mjoker.py
+++ mjoker.py
@@ -28,13 +28,6 @@ async def on_ready():
 	return await client.change_presence(game=discord.Game(name='Master play bots | 9679 users')) #add_your_bot_status_here
 
 
-@client.command(pass_context = True)
-@commands.has_permissions(administrator=True) 
-async def announce(ctx, channel: discord.Channel=None, *, msg: str):
-    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
-    embed=discord.Embed(title="Announcement", description="{}".format(msg), color = discord.Color((r << 16) + (g << 8) + b))
-    await client.send_message(channel, embed=embed)
-    await client.delete_message(ctx.message)
 
 
 
