766b5a80e898c7ee0153565227aa83f0acd362cd
masterjoker989 <46088660+masterjoker989@users.noreply.github.com>
2018-12-23 22:17:34 +0530

    Update mjoker.py

--- mjoker.py
+++ mjoker.py
@@ -16,7 +16,7 @@ import aiohttp
 
 
 Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
-client = commands.Bot(description="MultiVerse Official Bot", command_prefix=commands.when_mentioned_or("!!"), pm_help = True)
+client = commands.Bot(description="joker Official Bot", command_prefix=commands.when_mentioned_or("mw!"), pm_help = True)
 client.remove_command('help')
 
 
