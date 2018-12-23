1755ec6a6847dff2445c570697439a0015712e14
masterjoker989 <46088660+masterjoker989@users.noreply.github.com>
2018-12-23 22:42:01 +0530

    Update mjoker.py

--- mjoker.py
+++ mjoker.py
@@ -41,39 +41,6 @@ async def announce(ctx, channel: discord.Channel=None, *, msg: str):
 
 
 
-@client.command(pass_context=True, aliases=['server'])
-@commands.has_permissions(kick_members=True)
-async def membercount(ctx, *args):
-    """
-    Shows stats and information about current guild.
-    ATTENTION: Please only use this on your own guilds or with explicit
-    permissions of the guilds administrators!
-    """
-    if ctx.message.channel.is_private:
-        await bot.delete_message(ctx.message)
-        return
 
-    g = ctx.message.server
-
-    gid = g.id
-    membs = str(len(g.members))
-    membs_on = str(len([m for m in g.members if not m.status == Status.offline]))
-    users = str(len([m for m in g.members if not m.bot]))
-    users_on = str(len([m for m in g.members if not m.bot and not m.status == Status.offline]))
-    bots = str(len([m for m in g.members if m.bot]))
-    bots_on = str(len([m for m in g.members if m.bot and not m.status == Status.offline]))
-    created = str(g.created_at)
-    
-    em = Embed(title="Membercount")
-    em.description =    "```\n" \
-                        "Members:   %s (%s)\n" \
-                        "  Users:   %s (%s)\n" \
-                        "  Bots:    %s (%s)\n" \
-                        "Created:   %s\n" \
-                        "```" % (membs, membs_on, users, users_on, bots, bots_on, created)
-
-    await client.send_message(ctx.message.channel, embed=em)
-    await client.delete_message(ctx.message)
-	
 
 client.run(os.getenv('Token'))
